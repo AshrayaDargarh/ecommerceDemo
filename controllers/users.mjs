5f3806cd859d4a9cb16697a03450d53d4a71bb97
Ashraya Dargarh <74854130+AshrayaDargarh@users.noreply.github.com>
2023-08-07 20:19:01 +0530

    Update users.mjs

--- controllers/users.mjs
+++ controllers/users.mjs
@@ -1,13 +1,9 @@
 import fs from "fs";
 import { User } from "../models/user.mjs";
-// const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-// const users = data.users;
-
 export const createUser=async(req, res) => {
     const user= await User(req.body)
     try{
         const doc=await user.save()
-        console.log(doc)
         res.status(201).json(doc);
     }
     catch(err)
@@ -19,7 +15,6 @@ export const createUser=async(req, res) => {
 export const getAllUsers=async(req, res) => {
     try{
         const users=await User.find()
-        console.log(users)
         res.status(201).json(users);
     }
     catch(err)
@@ -32,7 +27,6 @@ export const getUser=async(req,res)=>{
     const id=req.params.id;
     try{
         const doc=await User.findById({_id:id})
-        console.log(doc)
         res.status(201).json(doc);
     }
     catch(err)
@@ -45,7 +39,6 @@ export const replaceUser=async(req,res)=>{
     const id=req.params.id;
     try{
         const doc=await User.findOneAndReplace({_id:id},req.body,{new:true})
-        console.log(doc)
         res.status(201).json(doc);
     }
     catch(err)
@@ -57,7 +50,6 @@ export const updateUser=async(req,res)=>{
     const id=req.params.id;
     try{
         const doc=await User.findOneAndUpdate({_id:id},req.body,{new:true})
-        console.log(doc)
         res.status(201).json(doc);
     }
     catch(err)
@@ -70,7 +62,6 @@ export const deleteUser=async(req,res)=>{
     const id=req.params.id;
     try{
         const doc=await User.findOneAndDelete({_id:id})
-        console.log(doc)
         res.status(201).json(doc);
     }
     catch(err)
